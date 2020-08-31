61a2b88fb8b471a8d1ef08f343a399ed8b3ec15a
Postpuber <65922120+Postpuber@users.noreply.github.com>
2020-08-31 20:07:06 +0300

    Update 3proxyinstall.sh

--- 3proxyinstall.sh
+++ 3proxyinstall.sh
@@ -1,6 +1,6 @@
 version=0.8.13
-apt-get update && apt-get -y upgrade
-apt-get install gcc make git -y
+apt update && apt -y upgrade
+apt install gcc make git -y
 wget --no-check-certificate -O 3proxy-${version}.tar.gz https://github.com/z3APA3A/3proxy/archive/${version}.tar.gz
 tar xzf 3proxy-${version}.tar.gz
 cd 3proxy-${version}
@@ -9,12 +9,12 @@ cd src
 mkdir /etc/3proxy/
 mv 3proxy /etc/3proxy/
 cd /etc/3proxy/
-wget --no-check-certificate https://github.com/SnoyIatk/3proxy/raw/master/3proxy.cfg
+wget --no-check-certificate https://github.com/Postpuber/3proxy/raw/master/3proxy.cfg
 chmod 600 /etc/3proxy/3proxy.cfg
 mkdir /var/log/3proxy/
-wget --no-check-certificate https://github.com/SnoyIatk/3proxy/raw/master/.proxyauth
+wget --no-check-certificate https://github.com/Postpuber/3proxy/raw/master/.proxyauth
 chmod 600 /etc/3proxy/.proxyauth
 cd /etc/init.d/
-wget --no-check-certificate  https://raw.github.com/SnoyIatk/3proxy/master/3proxy
+wget --no-check-certificate  https://raw.github.com/Postpuber/3proxy/master/3proxy
 chmod  +x /etc/init.d/3proxy
 update-rc.d 3proxy defaults
